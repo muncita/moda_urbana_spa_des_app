0e64ac72a94e83e14e14acfd7adf26a878d02fc1
muncita <147207286+muncita@users.noreply.github.com>
2025-10-30 23:22:14 -0300

    se repararon errores

--- app/src/main/java/com/example/modaurbanaapp/MainActivity.kt
+++ app/src/main/java/com/example/modaurbanaapp/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.ShoppingCart
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
@@ -20,28 +28,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ModaUrbanaAppTheme(darkTheme = true) { // look & feel streetwear
+            ModaUrbanaAppTheme(darkTheme = true) {
                 ModaUrbanaApp()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ModaUrbanaApp() {
     val navController = rememberNavController()
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("MODA URBANA") },
                 actions = {
-                    IconButton(onClick = { /* TODO: search */ }) { Icon(Icons.Default.Search, null) }
-                    IconButton(onClick = { /* TODO: account */ }) { Icon(Icons.Outlined.Person, null) }
-                    IconButton(onClick = { /* TODO: cart */ }) { Icon(Icons.Outlined.ShoppingCart, null) }
+                    IconButton(onClick = { /* TODO: search */ }) {
+                        Icon(Icons.Filled.Search, contentDescription = "Buscar")
+                    }
+                    IconButton(onClick = { /* TODO: account */ }) {
+                        Icon(Icons.Outlined.Person, contentDescription = "Cuenta")
+                    }
+                    IconButton(onClick = { /* TODO: cart */ }) {
+                        Icon(Icons.Outlined.ShoppingCart, contentDescription = "Carrito")
+                    }
                 }
             )
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                AppNavGraph(navController = navController)
+            }
         }
-    ) { inner ->
-        AppNavGraph(navController = navController)
-    }
+    )
 }
